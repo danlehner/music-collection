b25bde8dfaf9109ad83a1a5a5b2eb1c40bff77ea
danlehner <lehnerdr@gmail.com>
2020-09-18 16:58:37 -0400

    creates collection controller and uses it in server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express() 
+const controllers = require('./controllers')
 
 const PORT = 3000
+
+// get route for homepage
+app.get('/', (req, res) => {
+  res.send('This is the homepage')
+})
+
+app.use('/controllers', controllers.collection)
+
+
+// post route for homepage redirects to collection 
 
 app.listen(PORT, () => {
   console.log(`Now listening on http://localhost:${PORT}`)
