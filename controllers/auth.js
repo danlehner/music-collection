311b2f721541358f99433167488f73c92b592b98
danlehner <lehnerdr@gmail.com>
2020-09-24 13:28:43 -0400

    ability to create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,31 @@ router.get("/register", (req, res) => {
   res.render("auth/register")
 })
 
+// create user 
+router.post("/register", async (req, res) => {
+  try {
+    
+    const foundUser = await db.User.findOne({ email: req.body.email })
+
+    if (foundUser) {
+      res.send({ message: "Account already exists"})
+    }
+
+    const salt = await bcrypt.genSalt(10)
+
+    const hash = await bcrypt.hash(req.body.password, salt)
+    req.body.password = hash
+
+    await db.User.create(req.body)
+
+    res.redirect('/login')
+
+  } catch (error) {
+    console.log(error)
+    res.send({ message: "Internal Service Error"})
+  }
+})
+
 // login form 
 router.get("/login", (req, res) => {
   res.render("auth/login")
